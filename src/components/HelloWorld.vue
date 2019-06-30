64560ff396f2f2436c09249c052c4fc131920f93
tominagamaya <maya.tominaga.9@gmail.com>
2019-06-30 21:03:36 +0900

    Add sass style

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,20 +27,21 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+    <div class="hello__footer">footer</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
     msg: String
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style scoped lang="scss">
 h3 {
   margin: 40px 0 0;
 }
@@ -54,5 +55,10 @@ li {
 }
 a {
   color: #42b983;
+}
+.hello {
+  &__footer {
+    color: #0000ff;
+  }
 }
 </style>
